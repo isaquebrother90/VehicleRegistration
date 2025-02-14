b670de92ded94d9cfec2ec6b894309a2e57cadc2
isaquebrother90 <improgramcode@gmail.com>
2025-02-13 22:43:39 -0300

    adiciona estrutura básica do projeto.

---
+++ backend/src/test/java/com/vehicleregistration/vehicle_registration/VehicleRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicleregistration.vehicle_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
