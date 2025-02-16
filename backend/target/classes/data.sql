17be6fce92b1453563ae45be5d3dd0637d896485
isaquebrother90 <improgramcode@gmail.com>
2025-02-16 03:21:57 -0300

    Revert "fix: corrige pastas"
    This reverts commit 2a118148635361e9445816f20badbf1b5bec92ea.

--- backend/target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO veiculo (veiculo, marca, ano, descricao, vendido, created, updated)
-VALUES
-('Mustang', 'Ford', 2020, 'Carro esportivo na cor preta', false, '2023-10-01 10:00:00', '2023-10-01 10:00:00'),
-('Civic', 'Honda', 2019, 'Carro sedan na cor prata', false, '2023-10-01 10:00:00', '2023-10-01 10:00:00'),
-('Corolla', 'Toyota', 2021, 'Carro sedan na cor branca', true, '2023-10-01 10:00:00', '2023-10-01 10:00:00'),
-('Gol', 'Volkswagen', 2018, 'Carro hatch na cor vermelha', false, '2023-10-01 10:00:00', '2023-10-01 10:00:00'),
-('Onix', 'Chevrolet', 2022, 'Carro hatch na cor azul', true, '2023-10-01 10:00:00', '2023-10-01 10:00:00');
